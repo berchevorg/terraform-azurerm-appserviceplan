4205f79b9d35db349fda850fee4bc7cdc44f256c
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2024-02-09 10:37:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example-resources2"
   location = "West Europe"
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example"
+  name                = "example_new_resource2"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   os_type             = "Linux"
