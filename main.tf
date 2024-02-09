da4da3c8e2719c6f7ee85d544144b3342b4bcba4
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2024-02-09 10:29:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,12 @@
 resource "azurerm_resource_group" "example" {
-  name     = "api-rg-pro1"
+  name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "api-appserviceplan-pro"
-  location            = azurerm_resource_group.example.location
+resource "azurerm_service_plan" "example" {
+  name                = "example"
   resource_group_name = azurerm_resource_group.example.name
-  kind                = "FunctionApp"
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
